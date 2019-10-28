92512a342db7beb24cb5d2ef69e621b4f3ba7894
Anna Parsons <parson49@mail.nmc.edu>
2019-10-28 09:56:11 -0400

    changes

--- FileIOArray/Program.cs
+++ FileIOArray/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace FileIOArray
 {
     /// <summary>
-    /// App to demonstrate File IO
+    /// App to Demonstrate File IO
     /// </summary>
     class Program
     {
